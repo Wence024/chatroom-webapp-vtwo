7f6ea8f7036af44be42e6e7469547e88b0ec8648
Wence <ewence08@gmail.com>
2024-10-16 10:07:54 +0800

    feat: Added routes for the different pages (incomplete).

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './components/Login';
+import Signup from './components/Signup';
 import Chat from './components/Chat';
 import Header from './components/Header';
 
@@ -10,10 +12,17 @@ const App: React.FC = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <Container fluid className="p-0">
-      <Header />
-      {user ? <Chat /> : <Login />}
-    </Container>
+    <Router>
+      <Container fluid className="p-0">
+        <Header />
+        <Routes>
+          <Route path="/" element={user ? <Chat /> : <Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Container>
+    </Router>
   );
 };
 
