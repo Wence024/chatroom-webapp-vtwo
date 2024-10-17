ad0c4872aab988f96667cb6f04a3236c32c6c178
Wence <ewence08@gmail.com>
2024-10-17 20:56:00 +0800

    style: Refined the layout's responsiveness.

--- src/components/ThreeColumnLayout.tsx
+++ src/components/ThreeColumnLayout.tsx
@@ -8,21 +8,21 @@ const ThreeColumnLayout: React.FC = () => {
   return (
     <Container fluid className='pt-4 h-100'>
       <Row>
-        <Col lg={3} className="d-none d-lg-block mb-3">
+        <Col xl={3} className="d-none d-xl-block mb-3">
           <Card className="h-100 bg-dark text-light">
             <Card.Body>
               <OnlineUsers />
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={12} lg={6} className="mb-3">
+        <Col sm={12} md={8} xl={6} className="mb-3">
           <Card className="h-100 bg-dark text-light">
             <Card.Body>
               <Chat />
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={12} md={6} lg={3}>
+        <Col sm={12} md={4} lg={4} xl={3}>
           <Card className="h-100 bg-dark text-light">
             <Card.Body>
               <Profile />
